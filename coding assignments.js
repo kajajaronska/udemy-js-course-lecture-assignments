ff731543afd875f305919a9b7c2f7012853ac01c
Kaja <43033818+kajajaronska@users.noreply.github.com>
2020-10-23 13:04:23 +0100

    Upload of 2 coding assignments
    Coding assignments related to lectures: 
    - Values & variables 
    - Data types

---
+++ coding assignments.js
@@ -0,0 +1,21 @@
+//Coding assignment 1 - Values and variables
+
+let country = 'Poland';
+let continent = 'Europe';
+let population = 40;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+// Coding assignment 2 - Data types
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
