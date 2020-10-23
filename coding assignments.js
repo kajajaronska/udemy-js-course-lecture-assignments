669c6dc6fda58772747214afa91fad1dcf68c102
Kaja <43033818+kajajaronska@users.noreply.github.com>
2020-10-23 17:49:39 +0100

    Adding coding assignment
    Coding assignment 4 - Basic operators

--- coding assignments.js
+++ coding assignments.js
@@ -1,7 +1,7 @@
 // JAVASCRIPT FUNDAMENTALS - PART 1
 
 //Coding assignment 1 - Values and variables
-
+/*
 // let country = 'Poland';
 // let continent = 'Europe';
 let population = 40;
@@ -29,6 +29,31 @@ const continent = 'Europe';
 
 console.log(country);
 
-// country = 'Austria'; // returns error - cannot reassing constant variable
+// country = 'Austria'; // returns error - cannot reassign constant variable
 console.log(country);
+
+*/
+// Coding assignment 4 - Basic operators
+
+let myCountry = 'Poland';
+let continent = 'Europe';
+let myCountryLanguage = 'Polish';
+
+let myCountryPopulation = 40;
+let halfMyCountryPopulation = myCountryPopulation / 2;
+console.log(halfMyCountryPopulation)
+
+myCountryPopulation++;
+console.log(myCountryPopulation);
+
+let finlandPopulation = 6;
+console.log(finlandPopulation > myCountryPopulation); // false - 41 is higher than 6; 
+
+let averagePopulation = 33;
+console.log(averagePopulation > myCountryPopulation); // false  - 41 is higher than 33; 
+
+let description = myCountry + ' is in ' + continent + ', and its ' + myCountryPopulation + ' million people speak ' + myCountryLanguage + '.'
+
+console.log(description);
+
 
