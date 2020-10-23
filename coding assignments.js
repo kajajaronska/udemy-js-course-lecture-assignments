c066b5202d83ddb5d98c04a97cb3fca673769b4e
Kaja <43033818+kajajaronska@users.noreply.github.com>
2020-10-23 15:29:39 +0100

    Add files via upload

--- coding assignments.js
+++ coding assignments.js
@@ -1,11 +1,13 @@
+// JAVASCRIPT FUNDAMENTALS - PART 1
+
 //Coding assignment 1 - Values and variables
 
-let country = 'Poland';
-let continent = 'Europe';
+// let country = 'Poland';
+// let continent = 'Europe';
 let population = 40;
 
-console.log(country);
-console.log(continent);
+// console.log(country);
+// console.log(continent);
 console.log(population);
 
 
@@ -16,6 +18,17 @@ let language;
 
 console.log(typeof isIsland);
 console.log(typeof population);
-console.log(typeof country);
+// console.log(typeof country);
 console.log(typeof language);
+
+// Coding assignment 3 - let, const and var
+
+language = 'English';
+const country = 'Poland';
+const continent = 'Europe';
+
+console.log(country);
+
+// country = 'Austria'; // returns error - cannot reassing constant variable
+console.log(country);
 
