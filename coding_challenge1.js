0167ac6ab36dfe6db2f2bc9afa2153f4eef608a6
Kaja <43033818+kajajaronska@users.noreply.github.com>
2020-10-29 23:00:49 +0000

    Coding Challenge 1
    Coding challenge 1 from JS Fundamentals - Part 2 - Functions

--- coding_challenge1.js
+++ coding_challenge1.js
@@ -1,31 +1,23 @@
-//SCENARIO 1
 
-// weight in kg; height in meter; 
 
-let markWeight1 = 78;
-let markHeight1 = 1.69;
+const calcAverage = (firstScore, secondScore, thirdScore) => {
+    return (firstScore + secondScore + thirdScore) / 3;
+}
 
-let johnWeight1 = 92;
-let johnHeight1 = 1.95;
+const avgDolphins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
 
-let calcMarkBMI1 = markWeight1 / markHeight1 ** 2;
-let calcJohnBMI1 = johnWeight1 / johnHeight1 ** 2;
+function checkWinner(teamScore1, teamScore2) {
 
-let markHigherBMI1 = calcMarkBMI1 > calcJohnBMI1;
-console.log(calcMarkBMI1, calcJohnBMI1); // returns 27.3 for Mark & 24.2 for John
-console.log(markHigherBMI1); // returns true 
+    if (teamScore1 >= 2 * teamScore2) {
+        console.log(`Koalas win( ${teamScore1} vs. ${teamScore2}.)`);
+    } else if ((teamScore2 >= 2 * teamScore1)) {
+        console.log(`Koalas win( ${teamScore2} vs. ${teamScore1}.)`);
+    } else {
+        console.log(`None of the teams win...`);
+    }
+}
 
-//SCENARIO 2
+checkWinner(avgKoalas, avgDolphins);
 
-let markWeight2 = 95;
-let markHeight2 = 1.88;
-
-let johnWeight2 = 85;
-let johnHeight2 = 1.76;
-
-let calcMarkBMI2 = markWeight2 / markHeight2 ** 2;
-let calcJohnBMI2 = johnWeight2 / johnHeight2 ** 2;
-
-let markHigherBMI2 = calcMarkBMI2 > calcJohnBMI2;
-console.log(calcMarkBMI2, calcJohnBMI2); // returns 26.9 for Mark & 27.4 for John
-console.log(markHigherBMI2); // returns false 
+checkWinner(500, 250);
